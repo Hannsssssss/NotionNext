69c48cc05f3272e4eafbb67fe05fedae1236e12a
Hannsssssss <Hannsssssss@gmail.com>
2024-08-27 22:00:44 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -14,7 +14,7 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享课程',
+  HEO_HERO_TITLE_1: '分享课程666666',
   HEO_HERO_TITLE_2: '与软件资源',
   HEO_HERO_TITLE_3: 'ZIIKUU.XYZ',
   HEO_HERO_TITLE_4: '新版上线',
